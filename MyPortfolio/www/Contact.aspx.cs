9cbb4fbf6f6c3ae44000cffcfa0616bace956b96
Iliyan Stoev <stoev1989@gmail.com>
2016-05-17 10:48:58 +0200

    ..
    ..

--- MyPortfolio/www/Contact.aspx.cs
+++ MyPortfolio/www/Contact.aspx.cs
@@ -40,14 +40,14 @@ public partial class Contact : System.Web.UI.Page
 
         theMessage.From = new MailAddress(senderTb.Text);
 
-        theMessage.AddTo("c20xe@mail.bg");
+        theMessage.AddTo("blabla@mail.bg");
 
         theMessage.Subject = subjectTb.Text;
 
         theMessage.Text = messageTb.Text;
 
-        string userName = "azure_c27c4fa0c170d44a26686cab79e3eab0@azure.com";
-        string password = "Iliyan89";
+        string userName = "";
+        string password = "";
 
         var credentials = new NetworkCredential(userName, password);
 
